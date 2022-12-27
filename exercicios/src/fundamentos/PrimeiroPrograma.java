64bb2010892916bced4faad978229aebf5f6d02c
Leonardo Augusto de Freitas <leonardo.freitas@hotmail.com.br>
2022-12-27 19:54:53 +0000

    ajuste de pastas

---
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package fundamentos;
+/**
+ * JAVADOC
+ * @author LeonardoAFreitas
+ *
+ */
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		System.out.println("ola mundo!");
+
+	}
+}
